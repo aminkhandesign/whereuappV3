a8a15bbbfc01a26f30b797cb27d231c89ce93dea
AKD <makart@live.co.uk>
2017-04-13 21:36:57 -0700

    REFINED FORM, CLEARS FIELDS WHEN CLOSED

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,43 @@
 window.onload = function(){
 
 //assigning elements to variables
-var loginForm = document.getElementById('loginForm');
+var loginFormContainer = document.getElementById('loginFormContainer');
+var signupFormContainer = document.getElementById('signupFormContainer');
 var signupForm = document.getElementById('signupForm');
+var loginForm = document.getElementById('loginForm');
 var loginButt = document.getElementById('loginButt');
 var signupButt = document.getElementById('signupButt');
 var closeLogin = document.getElementById('closeLogin');
 var closeSignup = document.getElementById('closeSignup');
 
 //event handlers for revealing hiding forms
+
+//firstly function for clearing data from closed forms
+function clearForm(myForm){
+  for (i in myForm){
+    if(myForm[i].value!="LOGIN" && myForm[i].value!="SIGN UP")
+    {myForm[i].value=""}
+  }
+}
+
+
 function revealLoginForm(event){
-  loginForm.classList.remove("noForm");
-  loginForm.classList.add("form")
+  loginFormContainer.classList.remove("noForm");
+  loginFormContainer.classList.add("form")
 }
 function closeLoginForm(event){
-  loginForm.classList.remove("form");
-  loginForm.classList.add("noForm")
+  loginFormContainer.classList.remove("form");
+  loginFormContainer.classList.add("noForm");
+  clearForm(loginForm);
 }
 function revealSignupForm(event){
-  signupForm.classList.remove("noForm");
-  signupForm.classList.add("form")
+  signupFormContainer.classList.remove("noForm");
+  signupFormContainer.classList.add("form")
 }
 function closeSignupForm(event){
-  signupForm.classList.remove("form");
-  signupForm.classList.add("noForm")
+  signupFormContainer.classList.remove("form");
+  signupFormContainer.classList.add("noForm");
+  clearForm(signupForm);
 }
 //applying event handlers
 loginButt.addEventListener("click",revealLoginForm);
